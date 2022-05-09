c52764ba54030b98a05206548185b96280f11c8f
rochmans26 <rochmansetiono26@gmail.com>
2022-05-09 21:46:50 +0700

    update home.php dan header.php di bagian views

--- application/views/headerfooter/header.php
+++ application/views/headerfooter/header.php
@@ -8,6 +8,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title><?= $judul; ?> | Jejaka Outdoor</title>
+    <title><?= $judul; ?> | CI-App</title>
   </head>
   <body>
+    <!-- Nav Bar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="#">CI-App</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+     <div class="navbar-nav">
+       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+       <a class="nav-item nav-link" href="#">User</a>
+       <a class="nav-item nav-link" href="#">About</a>
+     </div>
+    </div>
+  </nav>

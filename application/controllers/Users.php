d956234ed2c1e5345ffdba96626b8a089fdb4c0c
rochmans26 <rochmansetiono26@gmail.com>
2022-05-11 16:03:52 +0700

    mengubah beberapa file

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -10,7 +10,13 @@ class Users extends CI_Controller {
       $data['judul'] = 'Users';
       $data['list'] = $this->users_model->getalldata();
       $this->load->view("headerfooter/header.php", $data);
-      $this->load->view("users.php", $data);
+      $this->load->view("users/users.php", $data);
+      $this->load->view("headerfooter/footer.php");
+  }
+  public function add(){
+      $data['judul'] = 'Add Users Form';
+      $this->load->view("headerfooter/header.php", $data);
+      $this->load->view("users/add_user.php");
       $this->load->view("headerfooter/footer.php");
   }
 }
